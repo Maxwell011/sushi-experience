faabe2608bcb9dd14d6c899bb4f5815b2cda0e22
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-12-12 14:00:16 +0100

    Updated app imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import './App.css'
+import "./App.css";
+import Nav from "./components/Nav";
 
 function App() {
-
   return (
     <>
-      <section className="bg-food-bar">
-        <h1 className="text-light-green">Welcome to the best sushi restaurant</h1>
+      <Nav />
+      <section className='bg-food-bar'>
+        <h1 className='text-light-green'>
+          Welcome to the best sushi restaurant
+        </h1>
       </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
