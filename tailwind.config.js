1425c728a63cfa98f4bd930ac750611eb4bc3987
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-12-12 21:41:08 +0100

    Added inter fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,16 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        "poppins": ["Poppins", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
       },
     },
     colors: {
       "light-green": "#39DB4A",
       "mild-green": "#5FE26C",
       "food-bar": "#C1F1C6",
+      "normal-black": "#000",
+      "normal-white": "#fff",
     },
   },
   plugins: [require("daisyui")],
