0e082febff0a213abf0556d505732807b3916465
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-12-13 12:46:55 +0100

    Added icon and removed images

---
+++ constants/index.ts
@@ -0,0 +1,5 @@
+export const PEOPLE_URL = [
+  "/portrait-lovely-blond-female-part-time-employee-talking-manager-customer-support-removebg-preview 1.png",
+  "/image 10.png",
+  "/image 11.png",
+];
