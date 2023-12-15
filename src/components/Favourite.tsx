159eb1e694a1a241800f2fee434de78db1c2b585
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-12-15 12:59:27 +0100

    Update styling in Favourite and Nav components

--- src/components/Favourite.tsx
+++ src/components/Favourite.tsx
@@ -1,7 +1,7 @@
 const Favourite = () => {
   return (
     <>
-      <section className="flex flex-col gap-[50px]">
+      <section className='flex flex-col gap-[50px]'>
         <div className='flex flex-col justify-center gap-[10px]'>
           <h1 className='text-[#F63B3B] text-[19px] font-bold leading-[normal] tracking-[3.08px] font-inter text-center'>
             CUSTOMER FAVORITES
@@ -12,33 +12,61 @@ const Favourite = () => {
         </div>
 
         <div className='flex justify-center gap-[25px]'>
-          <div className='shadow-[2px_4px_30px_0px_rgba(0,0,0,0.10)] w-[260px] h-[250px] rounded-[40px] bg-normal-white flex items-center justify-center flex-col'>
+          <div className='shadow-[2px_4px_30px_0px_rgba(0,0,0,0.10)] w-[260px] h-[250px] rounded-[40px] bg-normal-white flex items-center justify-center flex-col gap-1'>
             <div className='bg-food-bar flex items-center justify-center rounded-[60px] w-[130px] h-[120px]'>
-              <img src='/popular-1' alt='popular-dish' />
+              <img
+                src='/_78d38fb4-559d-4cdb-822d-44ecdec0a7f8-removebg-preview 1.png'
+                alt='popular-dish'
+              />
             </div>
-            <h2>Main Dish</h2>
-            <p>(86 dishes)</p>
+            <h2 className='text-[#1E1E1E] text-center text-xl font-semibold font-inter'>
+              Main Dish
+            </h2>
+            <p className='text-[#555] text-center text-[15px] font-medium font-inter'>
+              (86 dishes)
+            </p>
           </div>
-          <div className='shadow-[2px_4px_30px_0px_rgba(0,0,0,0.10)] w-[260px] h-[250px] rounded-[40px] bg-normal-white flex items-center justify-center flex-col'>
+          <div className='shadow-[2px_4px_30px_0px_rgba(0,0,0,0.10)] w-[260px] h-[250px] rounded-[40px] bg-normal-white flex items-center justify-center flex-col gap-1'>
             <div className='bg-food-bar flex items-center justify-center rounded-[60px] w-[130px] h-[120px]'>
-              <img src='/popular-2' alt='popular-dish' />
+              <img
+                src='/_35e8e57c-d430-4c19-93de-71e2eb204d0f-removebg-preview 1.png'
+                alt='popular-dish'
+              />
             </div>
-            <h2>Breakfast</h2>
-            <p>(12 dishes)</p>
+            <h2 className='text-[#1E1E1E] text-center text-xl font-semibold font-inter'>
+              Breakfast
+            </h2>
+            <p className='text-[#555] text-center text-[15px] font-medium font-inter'>
+              (12 dishes)
+            </p>
           </div>
-          <div className='shadow-[2px_4px_30px_0px_rgba(0,0,0,0.10)] w-[260px] h-[250px] rounded-[40px] bg-normal-white flex items-center justify-center flex-col'>
+          <div className='shadow-[2px_4px_30px_0px_rgba(0,0,0,0.10)] w-[260px] h-[250px] rounded-[40px] bg-normal-white flex items-center justify-center flex-col gap-1'>
             <div className='bg-food-bar flex items-center justify-center rounded-[60px] w-[130px] h-[120px]'>
-              <img src='/popular-3.png' alt='popular-dish' />
+              <img
+                src='/_a9c4460d-a7e5-496e-b46a-360fde90c966-removebg-preview 1.png'
+                alt='popular-dish'
+              />
             </div>
-            <h2>Sushi Combo</h2>
-            <p>(combo of 6)</p>
+            <h2 className='text-[#1E1E1E] text-center text-xl font-semibold font-inter'>
+              Sushi Combo
+            </h2>
+            <p className='text-[#555] text-center text-[15px] font-medium font-inter'>
+              (combo of 6)
+            </p>
           </div>
-          <div className='shadow-[2px_4px_30px_0px_rgba(0,0,0,0.10)] w-[260px] h-[250px] rounded-[40px] bg-normal-white flex items-center justify-center flex-col'>
+          <div className='shadow-[2px_4px_30px_0px_rgba(0,0,0,0.10)] w-[260px] h-[250px] rounded-[40px] bg-normal-white flex items-center justify-center flex-col gap-1'>
             <div className='bg-food-bar flex items-center justify-center rounded-[60px] w-[130px] h-[120px]'>
-              <img src='/popular-4' alt='popular-dish' />
+              <img
+                src='/_cc226cf5-2147-430e-b16c-84644636b1fe-removebg-preview 1.png'
+                alt='popular-dish'
+              />
             </div>
-            <h2>Browse All</h2>
-            <p>(255 items)</p>
+            <h2 className='text-[#1E1E1E] text-center text-xl font-semibold font-inter'>
+              Browse All
+            </h2>
+            <p className='text-[#555] text-center text-[15px] font-medium font-inter'>
+              (255 items)
+            </p>
           </div>
         </div>
       </section>
