2d615c1f7562791ea72bded0494c0cbccba25aa9
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-12-18 22:51:14 +0100

    Add like functionality to SpecialDishes component

--- src/components/SpecialDishes.tsx
+++ src/components/SpecialDishes.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { SPECIAL_DISHES } from "../../constants/index";
 import ArrowIcon from "../svg/ArrowIcon";
 import LoveIcon from "../svg/LoveIcon";
@@ -56,10 +57,16 @@ const DishesItem = ({
   price,
   rating,
 }: DishesItem) => {
+  const [liked, setLiked] = useState(false);
+
+  const toggleLike = () => {
+    setLiked(!liked);
+  };
+
   return (
     <div className='shadow-[2px_9px_42px_0px_rgba(0,0,0,0.12)] w-[350px] h-[450px] rounded-[40px] flex items-center flex-col justify-center'>
       <div className='w-20 h-[50px] absolute ml-[272px] mb-[400px] p-0 rounded-[0px_37.5px] bg-light-green flex items-center justify-center'>
-        <LoveIcon />
+        <LoveIcon onClick={toggleLike} liked={liked} />
       </div>
       <img src={image} alt={title} />
       <div className='flex flex-col items-start gap-1'>
