62d6c630d1b6fb94e183d3a4306bc08a2bebe08c
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-12-09 23:52:31 +0100

    Added extra dish

--- src/App.tsx
+++ src/App.tsx
@@ -16,22 +16,23 @@ function App() {
         <section className="">
           <div className="flex items-center mb-[-190px] mt-5">
             <div className="mb-[320px]">
-              <h1 className="text-normal-black text-[50px] font-extrabold font-inter w-[600px]">
+              <h1 className="text-normal-black text-5xl font-extrabold font-inter w-[600px]">
+                {/* <h1 className="text-normal-black text-5xl font-extrabold font-inter w-[600px]"> */}
                 Sushi Bliss, Every <br />
                 Roll A Flavorful{" "}
                 <span className="text-light-green text-[62px] font-extrabold">
                   kiss
                 </span>
               </h1>
-              <p className="text-[#4A4A4A] text-[20px] not-italic font-normal font-inter w-[825px]">
+              <p className="text-[#4A4A4A] text-lg not-italic font-normal font-inter w-[825px]">
                 Masters of Maki: Elevate Your Taste Buds with Our Sushi <br />
                 Creations, Where Tradition Meets Innovation!
               </p>
               <div className="flex items-center gap-[20px]">
-                <button className="w-[155px] h-[50px] shadow-[-2px_10px_50px_0px_rgba(57,219,74,0.50)] rounded-[40px] bg-light-green text-normal-white text-[20px]">
+                <button className="w-[140px] h-[50px] shadow-[-2px_10px_50px_0px_rgba(57,219,74,0.50)] rounded-[40px] bg-light-green text-normal-white text-lg">
                   Order Now
                 </button>
-                <div className="text-[#606060] text-[20px] font-semibold">
+                <div className="text-[#606060] text-lg font-semibold">
                   Watch Video
                 </div>
                 <div>
--- src/components/Favourite.tsx
+++ src/components/Favourite.tsx
@@ -12,7 +12,7 @@ export default function Favorite() {
         </div>
 
         <div className="flex justify-center gap-[25px] flex-col sm:flex-row">
-          <div className="shadow-[2px_4px_30px_0px_rgba(0,0,0,0.10)] w-[250px] h-[240px] rounded-[40px] bg-normal-white flex items-center justify-center flex-col gap-1">
+          <div className="shadow-[2px_4px_30px_0px_rgba(0,0,0,0.10)]  w-[230px]  h-[240px] rounded-[40px] bg-normal-white flex items-center justify-center flex-col gap-1">
             <div className="bg-food-bar flex items-center justify-center rounded-[60px] w-[130px] h-[120px]">
               <img
                 src="/_78d38fb4-559d-4cdb-822d-44ecdec0a7f8-removebg-preview 1.png"
@@ -26,7 +26,7 @@ export default function Favorite() {
               (86 dishes)
             </p>
           </div>
-          <div className="shadow-[2px_4px_30px_0px_rgba(0,0,0,0.10)]  w-[250px] h-[240px] rounded-[40px] bg-normal-white flex items-center justify-center flex-col gap-1">
+          <div className="shadow-[2px_4px_30px_0px_rgba(0,0,0,0.10)]   w-[230px]  h-[240px] rounded-[40px] bg-normal-white flex items-center justify-center flex-col gap-1">
             <div className="bg-food-bar flex items-center justify-center rounded-[60px] w-[130px] h-[120px]">
               <img
                 src="/_35e8e57c-d430-4c19-93de-71e2eb204d0f-removebg-preview 1.png"
@@ -40,7 +40,7 @@ export default function Favorite() {
               (12 dishes)
             </p>
           </div>
-          <div className="shadow-[2px_4px_30px_0px_rgba(0,0,0,0.10)]  w-[250px] h-[240px] rounded-[40px] bg-normal-white flex items-center justify-center flex-col gap-1">
+          <div className="shadow-[2px_4px_30px_0px_rgba(0,0,0,0.10)]   w-[230px]  h-[240px] rounded-[40px] bg-normal-white flex items-center justify-center flex-col gap-1">
             <div className="bg-food-bar flex items-center justify-center rounded-[60px] w-[130px] h-[120px]">
               <img
                 src="/_a9c4460d-a7e5-496e-b46a-360fde90c966-removebg-preview 1.png"
@@ -55,7 +55,7 @@ export default function Favorite() {
               (combo of 6)
             </p>
           </div>
-          <div className="shadow-[2px_4px_30px_0px_rgba(0,0,0,0.10)] w-[250px] h-[240px] rounded-[40px] bg-normal-white flex items-center justify-center flex-col gap-1">
+          <div className="shadow-[2px_4px_30px_0px_rgba(0,0,0,0.10)]  w-[230px]  h-[240px] rounded-[40px] bg-normal-white flex items-center justify-center flex-col gap-1">
             <div className="bg-food-bar flex items-center justify-center rounded-[60px] w-[130px] h-[120px]">
               <img
                 src="/_cc226cf5-2147-430e-b16c-84644636b1fe-removebg-preview 1.png"
