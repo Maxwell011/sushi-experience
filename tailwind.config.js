0e389df4cf3fce6f939549d825fafa4964f04651
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-12-19 23:23:33 +0100

    updated REadme file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
-        inter: ["Inter", "sans-serif"],
-      },
-    },
-    colors: {
-      "light-green": "#39DB4A",
-      "mild-green": "#5FE26C",
-      "food-bar": "#C1F1C6",
-      "normal-black": "#000",
-      "normal-white": "#fff",
-    },
-  },
-  plugins: [require("daisyui")],
-};
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         poppins: ["Poppins", "sans-serif"],
+//         inter: ["Inter", "sans-serif"],
+//       },
+//     },
+//     colors: {
+//       "light-green": "#39DB4A",
+//       "mild-green": "#5FE26C",
+//       "food-bar": "#C1F1C6",
+//       "normal-black": "#000",
+//       "normal-white": "#fff",
+//     },
+//   },
+//   plugins: [require("daisyui")],
+// };
