b39dddfb2b4d5030db5dcaf0dfe05245ed2bb799
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-12-21 23:18:50 +0100

    Added more content to the page

--- src/svg/ArrowIcon.tsx
+++ src/svg/ArrowIcon.tsx
@@ -1,41 +1,41 @@
-const ArrowIcon = () => {
-  return (
-    <div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="120"
-        height="80"
-        viewBox="0 0 210 80"
-        fill="none"
-      >
-        <circle cx="40" cy="40" r="40" fill="#EFEFEF" />
-        <path
-          d="M41.7646 32L34.0002 39.7645"
-          stroke="#6F6E6E"
-          strokeWidth="4"
-          strokeLinecap="round"
-        />
-        <path
-          d="M41.7646 48L34.0002 40.2355"
-          stroke="#6F6E6E"
-          strokeWidth="4"
-          strokeLinecap="round"
-        />
-        <circle cx="170" cy="40" r="40" fill="#39DB4A" />
-        <path
-          d="M166 32L173.764 39.7645"
-          stroke="white"
-          strokeWidth="4"
-          strokeLinecap="round"
-        />
-        <path
-          d="M166 48L173.764 40.2355"
-          stroke="white"
-          strokeWidth="4"
-          strokeLinecap="round"
-        />
-      </svg>
-    </div>
-  );
-};
-export default ArrowIcon;
+// const ArrowIcon = () => {
+//   return (
+//     <div>
+//       <svg
+//         xmlns="http://www.w3.org/2000/svg"
+//         width="120"
+//         height="80"
+//         viewBox="0 0 210 80"
+//         fill="none"
+//       >
+//         <circle cx="40" cy="40" r="40" fill="#EFEFEF" />
+//         <path
+//           d="M41.7646 32L34.0002 39.7645"
+//           stroke="#6F6E6E"
+//           strokeWidth="4"
+//           strokeLinecap="round"
+//         />
+//         <path
+//           d="M41.7646 48L34.0002 40.2355"
+//           stroke="#6F6E6E"
+//           strokeWidth="4"
+//           strokeLinecap="round"
+//         />
+//         <circle cx="170" cy="40" r="40" fill="#39DB4A" />
+//         <path
+//           d="M166 32L173.764 39.7645"
+//           stroke="white"
+//           strokeWidth="4"
+//           strokeLinecap="round"
+//         />
+//         <path
+//           d="M166 48L173.764 40.2355"
+//           stroke="white"
+//           strokeWidth="4"
+//           strokeLinecap="round"
+//         />
+//       </svg>
+//     </div>
+//   );
+// };
+// export default ArrowIcon;
