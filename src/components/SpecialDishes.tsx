70ec927fb9949d7db79fd87cdabdc91dbb2fc829
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-12-22 23:27:20 +0100

    Made the content responsive

--- src/components/SpecialDishes.tsx
+++ src/components/SpecialDishes.tsx
@@ -16,7 +16,7 @@ const SpecialDishes = () => {
             <h1 className="text-normal-black text-3xl font-bold w-[453px] font-inter">
               Best Dishes From Our Menu{" "}
             </h1>
-            <p>
+            <p className="hidden">
               <ArrowIcon />
             </p>
           </div>
