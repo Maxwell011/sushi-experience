6f0cde28cc263ee4248a17bb026552e9e2379f1f
Orji Maxwell <chukwuemekaorji678@gmail.com>
2024-12-31 22:47:47 +0100

    Uncomment footer section in App component for improved visibility and layout consistency

--- src/App.tsx
+++ src/App.tsx
@@ -110,13 +110,13 @@ function App() {
         <section>
           <Footer />
         </section>
-        {/* <div className="footer footer-center p-4 bg-normal-white text-base-content">
+        <div className="footer footer-center p-4 bg-normal-white text-base-content">
           <aside>
             <p className="text-xl">
               Copyright © 2023 - All right reserved by Sushi Food
             </p>
           </aside>
-        </div> */}
+        </div>
       </main>
     </>
   );
