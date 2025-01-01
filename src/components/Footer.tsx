dc135cfce61d635bbbfa9e48f3607f363ec454df
Orji Maxwell <chukwuemekaorji678@gmail.com>
2025-01-01 23:26:13 +0100

    Add header to Footer component for improved clarity

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,7 +14,9 @@ const Footer = () => {
           </p>
         </aside>
         <nav>
-          <header className="text-normal-black text-2xl not-italic font-semibold mb-[8px]"></header>
+          <header className="text-normal-black text-2xl not-italic font-semibold mb-[8px]">
+            Details
+          </header>
           <a className="link-hover text-[#555] text-base font-inter font-medium cursor-pointer">
             About us
           </a>
