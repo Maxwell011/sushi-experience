63fe7063a824bc97bf2394478e397c74dc83301b
Orji Maxwell <chukwuemekaorji678@gmail.com>
2025-01-02 19:38:28 +0100

    Update Footer component text size for improved readability

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
             </span>
             <span className="text-black text-2xl font-medium">ushi</span>
           </h1>
-          <p className="text-[#555] text-base not-italic font-medium w-[302px] font-inter">
+          <p className="text-[#555] text-sm not-italic font-medium w-[302px] font-inter">
             Savor the artistry taste where every dish is a culinary masterpiece.
           </p>
         </aside>
