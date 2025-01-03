c3d6da611aa0e5c2250ad189a1bc445458b787ab
Orji Maxwell <chukwuemekaorji678@gmail.com>
2025-01-03 23:40:07 +0100

    updated the root of the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
             Savor the artistry taste where every dish is a culinary masterpiece.
           </p>
         </aside>
-        <div className="flex gap-4">
+        <div className="flex gap-10">
           <nav className="flex flex-col">
             <header className="text-normal-black text-2xl not-italic font-semibold mb-[8px]">
               Details
